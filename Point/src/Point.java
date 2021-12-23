c80741bbb353ede323b41bfd143f645301107af3
Marat <94042173+Crescent17@users.noreply.github.com>
2021-12-23 12:16:28 +0300

    Add files via upload

---
+++ Point/src/Point.java
@@ -0,0 +1,41 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt((0.0 - (double) x) * (0.0 - (double) x) + (0.0 - (double) y) * (0.0 - (double) y));
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt((point.x - this.x) * (point.x - this.x) + (point.y - this.y) * (point.y - this.y));
+    }
+}
